40674d8ef916bf394d94f05912d135f3ab2890c4
Luk390 <lukebushell@hotmail.co.uk>
2022-05-05 07:08:33 +0100

    Added Adder

--- Integers/adder.go
+++ Integers/adder.go
@@ -1,1 +1,9 @@
 package main
+
+func Adder(xi ...int) int {
+	sum := 0
+	for _, v := range xi {
+		sum += v
+	}
+	return sum
+}
--- Integers/adder_test.go
+++ Integers/adder_test.go
@@ -3,5 +3,9 @@ package main
 import "testing"
 
 func TestAdder(t *testing.T) {
-
+	got := Adder(2, 2)
+	want := 4
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
 }
