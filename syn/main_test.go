1033ed51f28089132694beb750e620b19c5ec800
Luk390 <lukebushell@hotmail.co.uk>
2022-05-29 11:21:47 +0100

    finished

--- syn/main_test.go
+++ syn/main_test.go
@@ -1,7 +1,44 @@
 package main
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
-func TestXxx(t *testing.T) {
+func TestCounter(t *testing.T) {
+	
+	t.Run("increment counter 3 times leaves count at 3", func(t *testing.T) {
+		counter := NewCounter()
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+		assertCounter(t, counter, 3)
+	})
+	t.Run("runs safely when used concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
 
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func (){
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		assertCounter(t, counter, wantedCount)
+	})
+}
+
+func assertCounter(t testing.TB, got *Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
 }
