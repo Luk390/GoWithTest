e9d31569037c93a151a04be4500e38c1f218d518
Luk390 <lukebushell@hotmail.co.uk>
2022-08-31 08:38:25 +0100

    changes

--- sync/main_test.go
+++ sync/main_test.go
@@ -2,6 +2,18 @@ package main
 
 import "testing"
 
-func TestCounter(t *testing.T) {
-	
+func TestCounter (t *testing.T) {
+	t.Run("increment counter 3 times returns 3", func(t *testing.T){
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		got := counter.Value()
+		want := 3
+
+		if got != want {
+			t.ErrorF("got %v want %v", got, want)
+		}
+	})
 }
